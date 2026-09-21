f5ea74e590b16f9322572bb36839c24830b5b657
luisms123 <sistemacompu1@gmail.com>
2026-09-21 09:56:05 -0600

    guia de programacion

---
+++ gitUpload.sh
@@ -0,0 +1,5 @@
+cd C:/Users/compufismat/Downloads/RosadinTV2019/RosadinTV/Listas/epg/guia
+git add .
+git commit -m 'guia de programacion'
+git remote add origin https://github.com/luisms123/tdt
+git push --force origin master
